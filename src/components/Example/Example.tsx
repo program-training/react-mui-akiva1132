4063e7fc33ce26e003ea3498fb6d1de62363f28e
akiva_kraus <akiva1132@gmail.com>
2023-10-09 15:06:21 +0300

    ex 1-5

--- src/components/Example/Example.tsx
+++ src/components/Example/Example.tsx
@@ -1,12 +1,36 @@
 import { useState } from "react";
+import { Button, TextField, Checkbox, Radio, Select  } from "@mui/material";
 import "./Example.css";
+import { pink } from "@mui/material/colors";
 
 export function Example() {
-  const [state, setState] = useState(0);
+  const [inputText, setInputText] = useState("");
+
+  const handleButtonClick = () => {
+    const inputElement = document.getElementById('inputText') as HTMLInputElement;
+    if (inputElement) {
+      setInputText(inputElement.value);
+    }
+  };
+
   return (
-    <div className="example">
-      <h1>{state}</h1>
-      <button onClick={() => setState((prev) => prev + 1)}>Click me</button>
+    <div id="main">
+      <h1>{inputText}</h1>
+      <form>
+        <Button variant="contained" onClick={handleButtonClick}>
+          Display Text
+        </Button>
+        <TextField sx={{backgroundColor:"greenyellow"}} variant="filled" id="inputText" />
+        <Checkbox sx={{
+    color: pink[800],
+    '&.Mui-checked': {
+      color: pink[600],
+    },}}></Checkbox>
+        <Radio  />
+        <Select sx={{backgroundColor:"greenyellow"}}>
+          <option value="fv">dfv</option>
+        </Select>
+      </form>
     </div>
   );
 }
